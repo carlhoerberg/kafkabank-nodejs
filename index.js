1ba99bec282751580e182047e212b45178cf1d77
Carl Hörberg <carl.hoerberg@gmail.com>
2017-05-07 21:24:19 -0400

    dont auto commit offset and stop producer on ctrl-c

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 var balance = {};
 
+var Kafka = require('node-rdkafka');
+
+var consumer = new Kafka.KafkaConsumer({
+  'group.id': 'kafkabank-nodejs',
+  'metadata.broker.list': 'localhost:9092',
+  'socket.keepalive.enable': true,
+  'enable.auto.commit': false,
+}, {
+  'auto.offset.reset': 'beginning'
+});
+
+consumer
+  .on('ready', function() {
+    consumer.subscribe(['transactions']);
+    consumer.consume();
+  })
+  .on('data', function(data) {
+    var msg = JSON.parse(data.value);
+    if (!balance[msg.sender]) balance[msg.sender] = 0;
+    if (!balance[msg.receiver]) balance[msg.receiver] = 0;
+    balance[msg.sender] -= msg.amount;
+    balance[msg.receiver] += msg.amount;
+    console.log(balance);
+  });
+
+consumer.connect();
+
 var express = require('express');
 var app = express();
 
@@ -10,25 +37,9 @@ app.get('/balance/:account', function (req, res) {
 });
 app.listen(3000);
 
-var Kafka = require('node-rdkafka');
+process.on('SIGINT', function() {
+  console.log("Caught interrupt signal");
 
-var consumer = new Kafka.KafkaConsumer({
-    'group.id': 'kafkabank-nodejs',
-    'metadata.broker.list': 'localhost:9092',
-}, {});
-
-consumer
-  .on('ready', function() {
-    consumer.subscribe(['transactions']);
-    consumer.consume();
-  })
-  .on('data', function(data) {
-    console.log(data.message.toString());
-    var msg = data.message;
-    if (!balance[msg.sender]) balance[msg.sender] = 0;
-    if (!balance[msg.receiver]) balance[msg.receiver] = 0;
-    balance[msg.sender] -= msg.amount;
-    balance[msg.receiver] += msg.amount;
-  });
-
-consumer.connect();
+  consumer.disconnect();
+  process.exit();
+});
